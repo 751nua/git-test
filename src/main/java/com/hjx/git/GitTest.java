278c0252069ebe9219be1fdad64f05f7583279fa
hjx <hjx@atguigu.com>
2022-09-15 16:23:17 +0800

    second commit

--- src/main/java/com/hjx/git/GitTest.java
+++ src/main/java/com/hjx/git/GitTest.java
@@ -8,5 +8,6 @@ package com.hjx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hellogit2");
     }
 }
