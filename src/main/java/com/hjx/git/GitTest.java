464cb241445364b8ab4fa0d139b208e1bb00fbfa
hjx <hjx@atguigu.com>
2022-09-15 16:21:38 +0800

    first commit

---
+++ src/main/java/com/hjx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hjx.git;
+
+/**
+ * @Author:洪俊鑫
+ * @Time: 16:19 2022/9/15
+ * @Function:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+}
